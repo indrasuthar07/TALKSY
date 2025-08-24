cd289f0a91177d626b14ea63944da8426ba715b3
indar suthar <indrasuthar14@gmail.com>
2025-08-25 00:36:02 +0530

    adding backend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://talksy-chat-app.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
