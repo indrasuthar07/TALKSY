346df2de4e0f8282dd573cb9f7bec8c44d6fbcc0
indar suthar <indrasuthar14@gmail.com>
2025-08-25 00:51:04 +0530

    adjusting api paths

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://talksy-ashen.vercel.app/api" : "/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? "https://talksy-ashen.vercel.app/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"   // local dev backend
+    : "https://talksy-ashen.vercel.app/api"; // deployed backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
